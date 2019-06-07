ea3aa8259c0bdd6cf0b98b48e7cfcc7b27b27fcd
fatcat <fatcatchen@163.com>
2019-06-07 12:12:17 +0800

    add mappers

--- SourceCode/onlinebookstore/src/main/java/com/scut/onlinebookstore/OnlinebookstoreApplication.java
+++ SourceCode/onlinebookstore/src/main/java/com/scut/onlinebookstore/OnlinebookstoreApplication.java
@@ -1,8 +1,14 @@
 package com.scut.onlinebookstore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author FatCat
+ */
+
+@MapperScan("com.scut.onlinebookstore.DAO")
 @SpringBootApplication
 public class OnlinebookstoreApplication {
 
