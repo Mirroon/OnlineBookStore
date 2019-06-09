f49fd4b5f04aa0cb8ee1da4801f381c3a8e1e732
Mirroon <201630664406@mail.scut.edu.cn>
2019-06-09 14:18:33 +0800

    cache.

--- SourceCode/onlinebookstore/src/main/java/com/scut/onlinebookstore/controller/PersonControllers/VisitorController.java
+++ SourceCode/onlinebookstore/src/main/java/com/scut/onlinebookstore/controller/PersonControllers/VisitorController.java
@@ -1,10 +1,14 @@
 package com.scut.onlinebookstore.controller.PersonControllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author: 何健聪
  * @createTime: 2019/6/4 17:55
  * @version: 1.0
  * @description:
  */
+
 public class VisitorController {
+
 }
--- SourceCode/onlinebookstore/src/main/java/com/scut/onlinebookstore/controller/RootController.java
+++ SourceCode/onlinebookstore/src/main/java/com/scut/onlinebookstore/controller/RootController.java
@@ -1,12 +1,19 @@
 package com.scut.onlinebookstore.controller;
 
+import com.scut.onlinebookstore.controller.PersonControllers.VisitorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class RootController {
-    @RequestMapping("/")
+    @RequestMapping("hello")
     public String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("user")
+    public String user(){
+        return new VisitorController();
     }
 }
